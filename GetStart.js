45ae26c0048ac1c75ffc50fbde6f1fc4f8d64110
Gunasekar16082001 <68151191+Gunasekar16082001@users.noreply.github.com>
2023-01-13 14:16:42 +0530

    Add files via upload

---
+++ GetStart.js
@@ -0,0 +1,72 @@
+
+// variable
+
+let name = "variable sound";
+console.log(name);
+
+// constant
+
+const intreatRate =0.3;
+intreatRate = 1;
+console.log(intreatRate);
+
+//-------------- primary type(value type)--
+
+let name1 = "guna";//string
+let age = 30;//number
+let isApproved = true;//boolean
+let FirstName; //undefined
+let selectedColor = null;
+
+
+//Dynamic type--
+//typeof name =string
+
+//---------------------------------
+
+
+//---------- Reference Type--------------
+
+// Object
+
+let person ={
+    name2 : 'guna',
+    age1:10
+};
+console.log(person);
+
+
+//arrays
+
+let selectedColor1 = [ 'red' , 'blue' ];
+selectedColor1[2] = 'green';
+console.log(selectedColor1[0])
+ 
+
+
+// function
+
+function greet(name, lastname) {
+    console.log('hello' + name + '' +lastname) ; 
+ }
+ 
+ greet(' function guna', 'sekar');
+ greet('gu',"");
+ 
+
+ // calculating the value
+
+ function square(number){
+    return number * number;
+ }
+ console.log(square(2));
+
+
+ 
+
+
+
+
+
+
+
